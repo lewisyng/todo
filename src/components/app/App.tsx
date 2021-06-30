c3fc7f764adc4c0bf0a9993594d4368d9aa06079
Lewis Young <lewisyoung1306@yahoo.de>
2021-06-30 22:31:31 +0200

    deletedFooter

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -2,7 +2,6 @@ import { FunctionComponent } from "react";
 import "./App.sass";
 import Header from "../Header/Header";
 import Main from "../main/Main";
-import Footer from "../Footer/Footer";
 import { useDispatch } from "react-redux";
 import { useEffect } from "react";
 import { setUp } from "../../store/actions";
@@ -18,7 +17,6 @@ const App: FunctionComponent = () => {
     <div className="app">
       <Header />
       <Main />
-      <Footer />
     </div>
   );
 };
